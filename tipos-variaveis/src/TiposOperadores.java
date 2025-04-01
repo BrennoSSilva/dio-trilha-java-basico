6c1d156660ed10926f0b29e46d9f8b0237632ba4
Brenno S Silva <brennosoares2@gmail.com>
2025-03-31 21:08:56 -0300

    Iniciando os estuds (Operadores e Variaveis)

---
+++ tipos-variaveis/src/TiposOperadores.java
@@ -0,0 +1,24 @@
+public class TiposOperadores {
+    // classe Operadores.java
+    public static void main(String[] args) throws Exception {
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        
+        System.out.println(concatenacao);
+        
+        concatenacao = "1" + (1 + 1 + 1);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        String cep = "21070333"; // Alterado para String para evitar problemas com zeros à esquerda
+        String cpf = "98765432109"; // Alterado para String pelo mesmo motivo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        // Exibindo os valores
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("CEP: " + cep);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Pi: " + pi);
+        System.out.println("Salário: " + salario);
     }
 }
