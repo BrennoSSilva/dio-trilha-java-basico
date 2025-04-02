cd313951721ab41717571d6f7dbeb04cd37ca8c7
Brenno S Silva <brennosoares2@gmail.com>
2025-04-02 20:33:48 -0300

    Finalizando Operadordes .

---
+++ tipos-variaveis/src/TiposOperadores1.java
@@ -0,0 +1,15 @@
+public class TiposOperadores1 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 5;
+
+        String resultado = "";
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        System.out.println(resultado);
+    }
+    
+}
